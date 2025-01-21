9496a0c78ffbbb0a44a63cc7984ad73c61890fc4
Daniela <daniela.mjesus@senacsp.edu.br>
2025-01-21 18:55:15 -0300

    at

---
+++ EX008/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace EX008
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int numero;
+
+            Console.Write("digite um numero: ");
+            numero = Convert.ToInt32(Console.ReadLine());
+
+            if (numero % 2 == 0)
+            {
+                Console.WriteLine("O numero é par");
+            }
+            else
+            {
+                Console.WriteLine("O numero é impar");
+            }
+
+
+        }
+    }
+}
