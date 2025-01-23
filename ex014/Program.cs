03c55e862c02df608e98898fd0d2b3d0961f6f28
Daniela <daniela.mjesus@senacsp.edu.br>
2025-01-23 20:10:15 -0300

    exemplo1

---
+++ ex014/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace ex014
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i<=500; i++)
+            {
+                if(i % 3== 0)
+                {
+                    Console.Write("{0}, ", i);
+                }
+                
+            }
+        }
+    }
+}
