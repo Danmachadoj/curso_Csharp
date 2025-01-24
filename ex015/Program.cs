c84b0af0210ad81dae5303328830b528ad0bc80e
Daniela <daniela.mjesus@senacsp.edu.br>
2025-01-23 21:12:10 -0300

    tabuada

---
+++ ex015/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace ex015
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num;
+
+            Console.WriteLine("Digite um número");
+            num = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 1; i <= 10; i++)
+            {
+                Console.WriteLine("{0}x{1}={2}", num, i, num * i);                
+            }
+        }
+    }
+}
