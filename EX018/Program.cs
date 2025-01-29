fb9763d8a0bee1d36d9806adcf421ff9641dd290
Daniela <daniela.mjesus@senacsp.edu.br>
2025-01-28 21:38:40 -0300

    atualização senhas

---
+++ EX018/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace EX018
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string senha = "123";
+            string senhaDigitada;
+
+            do
+            {
+                Console.Write("Digite a senha: ");
+                senhaDigitada = Console.ReadLine();
+
+            } while (senhaDigitada != senha);
+
+            Console.WriteLine("Senha correta");
+
+
+
+
+        }
+    }
+}
