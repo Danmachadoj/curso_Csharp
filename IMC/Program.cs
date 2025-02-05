cdcfbcf4a28665557aeb8b29300cdef25bf4c26c
Daniela <daniela.mjesus@senacsp.edu.br>
2025-02-04 21:05:46 -0300

    imc up

---
+++ IMC/Program.cs
@@ -0,0 +1,42 @@
+﻿namespace IMC
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Digite seu peso: ");
+            double peso = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Digite sua altura: ");
+            double altura = Convert.ToDouble(Console.ReadLine());
+
+            double imc = peso / (altura * altura);
+            Console.WriteLine($"Seu IMC é: {imc:F2}");
+
+            if (imc < 18.5)
+            {
+                Console.WriteLine("Você está abaixo do peso.");
+            }
+            else if (imc >= 18.5 && imc < 24.9)
+            {
+                Console.WriteLine("Você está com peso normal.");
+            }
+            else if (imc >= 25 && imc < 29.9)
+            {
+                Console.WriteLine("Você está com sobrepeso.");
+            }
+            else if (imc >= 30 && imc < 34.9)
+            {
+                Console.WriteLine("Você está com obesidade grau 1.");
+            }
+            else if (imc >= 35 && imc < 39.9)
+            {
+                Console.WriteLine("Você está com obesidade grau 2.");
+            }
+            else
+            {
+                Console.WriteLine("Você está com obesidade grau 3 (obesidade mórbida).");
+            }
+        }
+    }
+}
