7fa34433c694b487aa31086016e91e000512bc37
Daniela <daniela.mjesus@senacsp.edu.br>
2025-02-04 22:19:28 -0300

    up

---
+++ Fatorial/Program.cs
@@ -0,0 +1,54 @@
+﻿namespace Fatorial
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            bool mostrar = false;
+
+            Console.WriteLine("Digite um número inteiro:");
+            int numero = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine();
+            Console.WriteLine("Deseja ver o calculo? (s/n)");
+            string resposta = Console.ReadLine().ToLower();
+            Console.WriteLine();
+
+            if (resposta == "s") { mostrar = true; }
+
+            Console.WriteLine(fatorial(numero, mostrar));
+        }
+
+        static int fatorial(int n, bool show = false)
+        {
+            int f = 1;
+            for (int i = n; i > 0; i--)
+            {                
+                if (show)
+                {
+                    Console.Write(i + " ");
+                    if (i > 1)
+                    {
+                        Console.Write("x ");
+                    }
+                    else
+                    {
+                        Console.Write("= ");
+                    }
+                }
+                f *= i;                
+            }
+            return f;
+        }
+    }
+}
+
+
+
+
+
+
+            
+              
+
+      
+      
